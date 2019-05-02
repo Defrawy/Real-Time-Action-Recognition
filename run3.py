6e77c1246acf2573c867a7aab0d87257aeb86c18
Mahmoud <Mahmoud@Mahmouds-MacBook-Air.local>
2019-05-02 15:00:35 +0200

    =running a video movie.mp4 instead of camera and save it to x.mp4

---
+++ run3.py
@@ -0,0 +1,177 @@
+# -*- coding: UTF-8 -*-
+from pose.estimator import TfPoseEstimator
+from pose.networks import get_graph_path
+from utils.sort import Sort
+from utils.actions import actionPredictor
+from utils.joint_preprocess import *
+import sys
+import cv2
+import numpy as np
+import time
+import settings
+
+poseEstimator = None
+
+
+def load_model():
+    global poseEstimator
+    poseEstimator = TfPoseEstimator(
+        get_graph_path('mobilenet_thin'), target_size=(432, 368))
+
+class Ui_MainWindow(object):
+
+    def __init__(self, parent=None):
+        #super(Ui_MainWindow, self).__init__(parent)
+        self.tracker = Sort(settings.sort_max_age, settings.sort_min_hit)
+        # self.timer_camera = QtCore.QTimer()
+        #self.cap = cv2.VideoCapture()
+        self.cap = cv2.VideoCapture('movie.mp4')
+        self.CAM_NUM = 0
+        #self.set_ui()
+        #self.slot_init()
+        self.__flag_mode = 3
+        self.fps = 0.00
+        self.data = {}
+        self.memory = {}
+        self.joints = []
+        self.current = []
+        self.previous = []
+
+        self.fourcc = cv2.VideoWriter_fourcc(*'mp4v') # Be sure to use lower case
+        self.out = cv2.VideoWriter('x.mp4', self.fourcc, 1, (432, 368))
+        # Define the codec and create VideoWriter object
+        
+
+
+    def show_camera(self, frame):
+        start = time.time()
+        # ret, frame = self.cap.read()
+        show = cv2.resize(frame, (settings.winWidth, settings.winHeight))
+        show = cv2.cvtColor(show, cv2.COLOR_BGR2RGB)
+        if ret:
+            if self.__flag_mode == 1:
+
+                humans = poseEstimator.inference(show)
+                show = TfPoseEstimator.draw_humans(show, humans, imgcopy=False)
+
+            elif self.__flag_mode == 2:
+                humans = poseEstimator.inference(show)
+                show, joints, bboxes, xcenter, sk = TfPoseEstimator.get_skeleton(show, humans, imgcopy=False)
+                height = show.shape[0]
+                width = show.shape[1]
+                if bboxes:
+                    result = np.array(bboxes)
+                    det = result[:, 0:5]
+                    det[:, 0] = det[:, 0] * width
+                    det[:, 1] = det[:, 1] * height
+                    det[:, 2] = det[:, 2] * width
+                    det[:, 3] = det[:, 3] * height
+                    trackers = self.tracker.update(det)
+
+                    for d in trackers:
+                        xmin = int(d[0])
+                        ymin = int(d[1])
+                        xmax = int(d[2])
+                        ymax = int(d[3])
+                        label = int(d[4])
+                        cv2.rectangle(show, (xmin, ymin), (xmax, ymax),
+                                      (int(settings.c[label % 32, 0]),
+                                       int(settings.c[label % 32, 1]),
+                                       int(settings.c[label % 32, 2])), 4)
+
+
+            # This is the part to get the kt points
+            elif self.__flag_mode == 3:
+                humans = poseEstimator.inference(show)
+                ori = np.copy(show)
+                show, joints, bboxes, xcenter, sk= TfPoseEstimator.get_skeleton(show, humans, imgcopy=False)
+                height = show.shape[0]
+                width = show.shape[1]
+                if bboxes:
+                    result = np.array(bboxes)
+                    det = result[:, 0:5]
+                    det[:, 0] = det[:, 0] * width
+                    det[:, 1] = det[:, 1] * height
+                    det[:, 2] = det[:, 2] * width
+                    det[:, 3] = det[:, 3] * height
+                    trackers = self.tracker.update(det)
+                    self.current = [i[-1] for i in trackers]
+
+                    if len(self.previous) > 0:
+                        for item in self.previous:
+                            if item not in self.current and item in self.data:
+                                del self.data[item]
+                            if item not in self.current and item in self.memory:
+                                del self.memory[item]
+
+                    self.previous = self.current
+                    for d in trackers:
+                        xmin = int(d[0])
+                        ymin = int(d[1])
+                        xmax = int(d[2])
+                        ymax = int(d[3])
+                        label = int(d[4])
+                        try:
+                            j = np.argmin(np.array([abs(i - (xmax + xmin) / 2.) for i in xcenter]))
+                        except:
+                            j = 0
+                        if joint_filter(joints[j]):
+                            joints[j] = joint_completion(joint_completion(joints[j]))
+                            if label not in self.data:
+                                self.data[label] = [joints[j]]
+                                self.memory[label] = 0
+                            else:
+                                self.data[label].append(joints[j])
+
+                            if len(self.data[label]) == settings.L:
+                                pred = actionPredictor().move_status(self.data[label])
+                                if pred == 0:
+                                    pred = self.memory[label]
+                                else:
+                                    self.memory[label] = pred
+                                self.data[label].pop(0)
+
+                                location = self.data[label][-1][1]
+                                if location[0] <= 30:
+                                    location = (51, location[1])
+                                if location[1] <= 10:
+                                    location = (location[0], 31)
+
+                                cv2.putText(show, settings.move_status[pred], (location[0] - 30, location[1] - 10),
+                                            cv2.FONT_HERSHEY_SIMPLEX, 0.8,
+                                            (0, 255, 0), 2)
+
+                        cv2.rectangle(show, (xmin, ymin), (xmax, ymax),
+                                      (int(settings.c[label % 32, 0]),
+                                       int(settings.c[label % 32, 1]),
+                                       int(settings.c[label % 32, 2])), 4)
+
+
+
+            cv2.imshow('window', cv2.resize(show, (432, 368)))
+            self.out.write(show) # Write out frame to video
+            end = time.time()
+          
+    def release(self):
+        self.out.release()
+
+	
+
+WIDTH, HEIGHT = 4, 3
+load_model()
+vidcap = cv2.VideoCapture('movie.mp4')
+height, width = int(vidcap.get(WIDTH)), int(vidcap.get(HEIGHT))
+ui = Ui_MainWindow()
+while(vidcap.isOpened()):
+	ret, image_np = vidcap.read()
+	ui.show_camera(image_np)
+	
+	if cv2.waitKey(25) & 0xFF == ord('q'):
+		break
+
+cv2.destroyAllWindows()
+
+
+
+
+
