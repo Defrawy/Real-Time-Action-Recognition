c1554a2b54c169559fb15b3aacb1cd7a9404c708
Mahmoud <Mahmoud@Mahmouds-MacBook-Air.local>
2019-05-04 20:30:10 +0200

    =merge all into run and add start and stop buttons

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 # -*- coding: UTF-8 -*-
 from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QFileDialog
 from pose.estimator import TfPoseEstimator
 from pose.networks import get_graph_path
 from utils.sort import Sort
@@ -12,7 +13,9 @@ import time
 import settings
 
 poseEstimator = None
-
+indicators = {
+    'camera': False,
+}
 
 def load_model():
     global poseEstimator
@@ -26,7 +29,9 @@ class Ui_MainWindow(QtWidgets.QWidget):
         super(Ui_MainWindow, self).__init__(parent)
         self.tracker = Sort(settings.sort_max_age, settings.sort_min_hit)
         self.timer_camera = QtCore.QTimer()
-        self.cap = cv2.VideoCapture()
+        # self.cap = cv2.VideoCapture()
+        # self.cap = cv2.VideoCapture()
+        self.cap = None
         self.CAM_NUM = 0
         self.set_ui()
         self.slot_init()
@@ -37,6 +42,8 @@ class Ui_MainWindow(QtWidgets.QWidget):
         self.joints = []
         self.current = []
         self.previous = []
+        self.fourcc = cv2.VideoWriter_fourcc(*'mp4v') # Be sure to use lower case
+        self.out = cv2.VideoWriter('x.mp4', self.fourcc, 1, (settings.winWidth, settings.winHeight))
 
     def set_ui(self):
 
@@ -44,25 +51,33 @@ class Ui_MainWindow(QtWidgets.QWidget):
         self.__layout_fun_button = QtWidgets.QVBoxLayout()
         self.__layout_data_show = QtWidgets.QVBoxLayout()
 
-        self.button_open_camera = QtWidgets.QPushButton(u'camera OFF')
+        self.button_open_camera = QtWidgets.QPushButton(u'Camera OFF')
 
         self.button_mode_1 = QtWidgets.QPushButton(u'Attitude Estimation OFF')
         self.button_mode_2 = QtWidgets.QPushButton(u'Multiplayer tracking OFF')
         self.button_mode_3 = QtWidgets.QPushButton(u'Behavior recognition OFF')
+        self.button_video = QtWidgets.QPushButton("Load Video")
 
-        self.button_close = QtWidgets.QPushButton(u'drop out')
+        self.button_start_playing = QtWidgets.QPushButton(u'Start')
+        self.button_stop_playing = QtWidgets.QPushButton(u'Stop')
+
+        self.button_close = QtWidgets.QPushButton(u'Exit')
 
         self.button_open_camera.setMinimumHeight(50)
         self.button_mode_1.setMinimumHeight(50)
         self.button_mode_2.setMinimumHeight(50)
         self.button_mode_3.setMinimumHeight(50)
+        self.button_video.setMinimumHeight(50)
+        self.button_start_playing.setMinimumHeight(50)
+        self.button_stop_playing.setMinimumHeight(50)
+
 
         self.button_close.setMinimumHeight(50)
 
         self.button_close.move(10, 100)
 
         self.infoBox = QtWidgets.QTextBrowser(self)
-        self.infoBox.setGeometry(QtCore.QRect(10, 300, 200, 180))
+        self.infoBox.setGeometry(QtCore.QRect(25, 435, 200, 180))
 
         # 信息显示
         self.label_show_camera = QtWidgets.QLabel()
@@ -76,6 +91,9 @@ class Ui_MainWindow(QtWidgets.QWidget):
         self.__layout_fun_button.addWidget(self.button_mode_1)
         self.__layout_fun_button.addWidget(self.button_mode_2)
         self.__layout_fun_button.addWidget(self.button_mode_3)
+        self.__layout_fun_button.addWidget(self.button_video)
+        self.__layout_fun_button.addWidget(self.button_start_playing)
+        self.__layout_fun_button.addWidget(self.button_stop_playing)
         self.__layout_fun_button.addWidget(self.button_close)
         self.__layout_fun_button.addWidget(self.label_move)
 
@@ -87,15 +105,20 @@ class Ui_MainWindow(QtWidgets.QWidget):
         self.setWindowTitle(u'Real-time multi-person attitude estimation and behavior recognition system')
 
     def slot_init(self):
-        self.button_open_camera.clicked.connect(self.button_event)
+        self.button_open_camera.clicked.connect(self.switch_to_camera)
         self.timer_camera.timeout.connect(self.show_camera)
 
-        self.button_mode_1.clicked.connect(self.button_event)
-        self.button_mode_2.clicked.connect(self.button_event)
-        self.button_mode_3.clicked.connect(self.button_event)
+        self.button_mode_1.clicked.connect(self.button_event2)
+        self.button_mode_2.clicked.connect(self.button_event2)
+        self.button_mode_3.clicked.connect(self.button_event2)
+        self.button_start_playing.clicked.connect(self.start_playing)
+        self.button_stop_playing.clicked.connect(self.stop_playing)
+        self.button_video.clicked.connect(self.getfile)
         self.button_close.clicked.connect(self.close)
 
-    def button_event(self):
+    
+    
+    def button_event2(self):
         sender = self.sender()
         if sender == self.button_mode_1 and self.timer_camera.isActive():
             if self.__flag_mode != 1:
@@ -132,31 +155,19 @@ class Ui_MainWindow(QtWidgets.QWidget):
             self.button_mode_1.setText(u'Attitude Estimation OFF')
             self.button_mode_2.setText(u'Multiplayer tracking OFF')
             self.button_mode_3.setText(u'Behavior recognition OFF')
-            if self.timer_camera.isActive() == False:
-                flag = self.cap.open(self.CAM_NUM)
-                self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, settings.winWidth)
-                self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, settings.winHeight)
-                if flag == False:
-                    msg = QtWidgets.QMessageBox.warning(self, u"Warning", u"Please check if the camera and computer are connected correctly.",
-                                                        buttons=QtWidgets.QMessageBox.Ok,
-                                                        defaultButton=QtWidgets.QMessageBox.Ok)
-                else:
-                    self.timer_camera.start(1)
-                    self.button_open_camera.setText(u'camera ON')
-                    self.infoBox.setText(u'camera已打开')
-            else:
-                self.timer_camera.stop()
-                self.cap.release()
-                self.label_show_camera.clear()
-                self.button_open_camera.setText(u'camera OFF')
-                self.infoBox.setText(u'Camera is off')
+            
 
     def show_camera(self):
         start = time.time()
         ret, frame = self.cap.read()
-        show = cv2.resize(frame, (settings.winWidth, settings.winHeight))
-        show = cv2.cvtColor(show, cv2.COLOR_BGR2RGB)
+        # if not ret:
+        #     print('this should save the video')
+        #     self.timer_camera.stop()
+        #     self.out.release()
+            
         if ret:
+            show_s = cv2.resize(frame, (settings.winWidth, settings.winHeight))
+            show = cv2.cvtColor(show_s, cv2.COLOR_BGR2RGB)
             if self.__flag_mode == 1:
                 self.infoBox.setText(u'当前为人体Attitude Estimation模式')
                 humans = poseEstimator.inference(show)
@@ -166,6 +177,7 @@ class Ui_MainWindow(QtWidgets.QWidget):
                 self.infoBox.setText(u'当前为Multiplayer tracking模式')
                 humans = poseEstimator.inference(show)
                 show, joints, bboxes, xcenter, sk = TfPoseEstimator.get_skeleton(show, humans, imgcopy=False)
+                show = TfPoseEstimator.draw_humans(show, humans, imgcopy=False)
                 height = show.shape[0]
                 width = show.shape[1]
                 if bboxes:
@@ -195,6 +207,7 @@ class Ui_MainWindow(QtWidgets.QWidget):
                 humans = poseEstimator.inference(show)
                 ori = np.copy(show)
                 show, joints, bboxes, xcenter, sk= TfPoseEstimator.get_skeleton(show, humans, imgcopy=False)
+                show = TfPoseEstimator.draw_humans(show, humans, imgcopy=False)
                 height = show.shape[0]
                 width = show.shape[1]
                 if bboxes:
@@ -256,22 +269,31 @@ class Ui_MainWindow(QtWidgets.QWidget):
                                        int(settings.c[label % 32, 1]),
                                        int(settings.c[label % 32, 2])), 4)
 
+            
+            self.out.write(show_s) # Write out frame to video
+
             end = time.time()
             self.fps = 1. / (end - start)
             cv2.putText(show, 'FPS: %.2f' % self.fps, (30, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
             showImage = QtGui.QImage(show.data, show.shape[1], show.shape[0], QtGui.QImage.Format_RGB888)
             self.label_show_camera.setPixmap(QtGui.QPixmap.fromImage(showImage))
+        else:
+            print('this should save the video')
+            self.timer_camera.stop()
+            self.out.release()
+
 
     def closeEvent(self, event):
+        # self.out.release()
         ok = QtWidgets.QPushButton()
         cancel = QtWidgets.QPushButton()
 
-        msg = QtWidgets.QMessageBox(QtWidgets.QMessageBox.Warning, u"shut down", u"Whether it is closed!")
+        msg = QtWidgets.QMessageBox(QtWidgets.QMessageBox.Warning, u"shut down", u"Are you sure you want to quit?")
 
         msg.addButton(ok, QtWidgets.QMessageBox.ActionRole)
         msg.addButton(cancel, QtWidgets.QMessageBox.RejectRole)
-        ok.setText(u'determine')
-        cancel.setText(u'cancel')
+        ok.setText(u'Yes')
+        cancel.setText(u'Cancel')
         if msg.exec_() == QtWidgets.QMessageBox.RejectRole:
             event.ignore()
         else:
@@ -281,6 +303,69 @@ class Ui_MainWindow(QtWidgets.QWidget):
                 self.timer_camera.stop()
             event.accept()
             print("System exited.")
+
+
+    def getfile(self):
+      dlg = QFileDialog()
+      dlg.setFileMode(QFileDialog.AnyFile)
+      fname = dlg.getOpenFileName(self, 'Open file', 
+         'c:\\')
+      # self.le.setPixmap(QPixmap(fname))
+      if fname:
+        self.load_video(fname[0])
+        
+   # def getfiles(self):
+   #    dlg = QFileDialog()
+   #    dlg.setFileMode(QFileDialog.AnyFile)
+   #    dlg.setFilter("Text files (*.txt)")
+   #    filenames = QStringList()
+        
+   #    if dlg.exec_():
+   #       filenames = dlg.selectedFiles()
+   #       f = open(filenames[0], 'r')
+            
+   #       with f:
+   #          data = f.read()
+   #          self.contents.setText(data)
+
+    def load_video(self, name):
+        self.cap = cv2.VideoCapture(name)
+        # self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, settings.winWidth)
+        # self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, settings.winHeight)
+        self.timer_camera.stop()
+        # self.timer_camera.start(1)
+
+    def switch_to_camera(self):
+        indicators['camera'] = not indicators['camera'] 
+        [self.stop_camera, self.start_camera][int(indicators['camera'])]()
+        self.button_open_camera.setText(
+            ['Camera OFF', 'Camera ON'][int(indicators['camera'])]
+        )
+        
+
+    def stop_camera(self):
+        # self.out.release()
+        self.cap.release()
+        self.timer_camera.stop()
+
+    def start_camera(self):
+        self.cap = cv2.VideoCapture(0)
+        # self.timer_camera.stop()
+        # self.timer_camera.start(1)
+
+    def start_playing(self):
+        self.timer_camera.start(1)
+
+    def stop_playing(self):
+        self.timer_camera.stop()
+        self.out.release()
+
+    def save_video(self):
+        name = QtGui.QFileDialog.getSaveFileName(self, 'Save File')
+        # file = open(name,'w')
+        # text = self.textEdit.toPlainText()
+        # file.write(text)
+        # file.close()
 
 
 if __name__ == '__main__':
